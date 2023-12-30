25cb3408653c70ba798c005564b4c30aa19698f1
Steven <espisteven5@gmail.com>
2023-12-29 20:25:20 -0800

    initial

--- lib/enrollment.py
+++ lib/enrollment.py
@@ -1,23 +1,39 @@
 from datetime import datetime
+
 class Student:
     def __init__(self, name):
         self.name = name
         self._enrollments = []
+        self._grades = {}  
 
     def enroll(self, course):
         if isinstance(course, Course):
             enrollment = Enrollment(self, course)
             self._enrollments.append(enrollment)
             course.add_enrollment(enrollment)
+            self._grades[enrollment] = None  
         else:
             raise TypeError("course must be an instance of Course")
 
     def get_enrollments(self):
         return self._enrollments.copy()
 
+    def course_count(self):  
+        return len(self._enrollments)
+
+    def set_grade(self, enrollment, grade):  
+        if enrollment in self._enrollments:
+            self._grades[enrollment] = grade
+        else:
+            raise ValueError("Enrollment not found for this student")
+
+    def aggregate_average_grade(self):  
+        total_grades = sum(grade for grade in self._grades.values() if grade is not None)
+        num_courses = len([grade for grade in self._grades.values() if grade is not None])
+        return total_grades / num_courses if num_courses > 0 else 0
+
 class Course:
     def __init__(self, title):
-
         self.title = title
         self._enrollments = []
 
@@ -30,7 +46,6 @@ class Course:
     def get_enrollments(self):
         return self._enrollments.copy()
 
-
 class Enrollment:
     all = []
     
@@ -39,9 +54,17 @@ class Enrollment:
             self.student = student
             self.course = course
             self._enrollment_date = datetime.now()
-            type(self).all.append(self)
+            Enrollment.all.append(self)
         else:
             raise TypeError("Invalid types for student and/or course")
 
     def get_enrollment_date(self):
         return self._enrollment_date
+
+    @classmethod
+    def aggregate_enrollments_per_day(cls):  
+        enrollment_count = {}
+        for enrollment in cls.all:
+            date = enrollment.get_enrollment_date().date()
+            enrollment_count[date] = enrollment_count.get(date, 0) + 1
+        return enrollment_count
